26fc15186841e170076f3e5f64992e6091a891df
Louis.chen <louis.chen@finsage.com.tw>
2017-09-22 11:15:54 +0800

    FastJsonInitializer arrange

--- src/main/java/com/finsage/App.java
+++ src/main/java/com/finsage/App.java
@@ -22,42 +22,10 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
  * @version v.0.1
  * @date 2017.09.13
  */
-@SpringBootApplication
-// @ServletComponentScan // 啟動 servlet 掃描。
+@SpringBootApplication // same as @Configuration, @EnableAutoConfiguration, @ComponentScan
 @ComponentScan(basePackages = { "com.finsage" }) // 啟動包目錄
 @MapperScan(basePackages = { "com.finsage.*.*.mapper", "com.finsage.*.mapper" }) // 掃描該包下相應的 class ，主要是 MyBatis 持久化類
 public class App extends WebMvcConfigurerAdapter {
-
-	/**
-	 * Fast Jason
-	 */
-	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		super.configureMessageConverters(converters);
-		/*
-		 * 1. 需要先定義一個 convert 轉換消息的對象; 2. 添加 fastJson 的配置訊息。比如：是否要格式化回傳 json數據;
-		 * 3. 在 convert 中添加配置訊息; 4. 將 convert 添加到 converters 當中;
-		 */
-
-		// 1. 需要先定義一個 convert 轉換消息的對象;
-		FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-		// 2. 添加 fastJson 的配置訊息。比如：是否要格式化回傳 json數據;
-		FastJsonConfig fastJsonConfig = new FastJsonConfig();
-		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-		// 处理中文乱码问题
-		List<MediaType> fastMediaTypes = new ArrayList<>();
-		fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-		fastConverter.setSupportedMediaTypes(fastMediaTypes);
-		// 3. 在 convert 中添加配置訊息;
-		fastConverter.setFastJsonConfig(fastJsonConfig);
-		// 4. 將 convert 添加到 converters 當中;
-		converters.add(fastConverter);
-	}
-
-	// @Bean
-	// public ServletRegistrationBean myServlet1() {
-	// return new ServletRegistrationBean(new MyServlet1(), "/myServlet1");
-	// }
 
 	public static void main(String[] args) throws Exception {
 		/*
@@ -65,6 +33,8 @@ public class App extends WebMvcConfigurerAdapter {
 		 */
 		System.out.println("<<<<<  Spring Boot!!!  >>>>>");
 		SpringApplication.run(App.class, args);
+		// 熱部署(關閉)
+//		System.setProperty("spring.devtools.restart.enabled", "false");
 	}
 
 }
--- src/main/java/com/finsage/util/config/ServletInitializer.java
+++ src/main/java/com/finsage/util/config/ServletInitializer.java
@@ -1,9 +1,18 @@
-package com.finsage;
+package com.finsage.util.config;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-public class ServletInitlalizer extends SpringBootServletInitializer {
+import com.finsage.App;
+
+/**
+ * Servlet 初始化，搭配可進行 jboss 部署
+ * 
+ * @author yuan
+ * @version v.0.1
+ * @date 2017.09.20
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
